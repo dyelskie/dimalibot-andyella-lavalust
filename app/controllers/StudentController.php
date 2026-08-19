ba12bb3a9b58b4fa671164ff2b7c597b5898b599
Andyella Dimalibot <andyellaud@gmail.com>
2026-08-19 10:48:28 +0800

    Initial LavaLust project

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,14 @@
+<?php 
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed'); 
+class StudentController extends Controller 
+{ 
+    public function index() 
+    { 
+        // Display student page 
+    } 
+    
+    public function profile() 
+    { 
+        // Display student profile
+    } 
+} 
